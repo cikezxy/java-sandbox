1f10d13347a3db35d99b9b79f7426e5abd68d354
cikezxy <cikezxy@163.com>
2017-11-23 21:58:14 +0800

    bean utils test

--- README.md
+++
@@ -1,2 +0,0 @@
-# java-sandbox
-a sandbox for java-api, java 3rd libraries and etc.
---
+++ src/test/java/com/cikezxy/sandbox/beanutils/JacksonBeanUtilsTest.java
@@ -0,0 +1,12 @@
+package com.cikezxy.sandbox.beanutils;
+
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import org.junit.Test;
+
+public class JacksonBeanUtilsTest {
+
+    @Test
+    public void testJacksonBeanUtil() {
+
+    }
+}
