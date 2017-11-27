369d99e76f56ac22cc73712b6fa6bc4cfcfe84cc
cikezxy <cikezxy@163.com>
2017-11-27 22:14:59 +0800

    bean utils test

---
+++ src/main/java/jmx/HelloAgent.java
@@ -0,0 +1,15 @@
+package jmx;
+
+import javax.management.*;
+import java.lang.management.ManagementFactory;
+
+public class HelloAgent {
+
+    public static void main(String[] args) throws MalformedObjectNameException, InterruptedException, NotCompliantMBeanException, InstanceAlreadyExistsException, MBeanRegistrationException {
+        MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
+        ObjectName helloName = new ObjectName("cikezxy:name=HelloWorld");
+        mBeanServer.registerMBean(new Hello(), helloName);
+        System.out.println("Waiting forever...");
+        Thread.sleep(Long.MAX_VALUE);
+    }
+}
