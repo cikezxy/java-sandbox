df5077446c8e9a81f6a75513e159881b74ca70ff
cikezxy <cikezxy@163.com>
2018-09-19 05:47:41 +0800

    instanceof isinstance isassignablefrom

--- src/test/java/com/cikezxy/sandbox/java/java8/StreamBasicTest.java
+++ src/test/java/com/cikezxy/sandbox/java/java8/StreamBasicTest.java
@@ -5,8 +5,13 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.TreeMap;
+import java.util.function.Function;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.cikezxy.sandbox.java8.stream.Dish;
@@ -22,104 +27,94 @@ public class StreamBasicTest {
             new Dish("season fruit", true, 120, Dish.Type.OTHER),
             new Dish("pizza", true, 550, Dish.Type.OTHER),
             new Dish("prawns", false, 300, Dish.Type.FISH),
-            new Dish("salmon", false, 450, Dish.Type.FISH) );
-    @Test
-    public void testGetLowCaloricDishesName() {
-        List<String> names = menu.stream().filter(dish -> dish.getCalories() < 400)
-                .sorted(Comparator.comparing(Dish::getCalories)).map(Dish::getName).collect(Collectors.toList());
+            new Dish("salmon", false, 450, Dish.Type.FISH));
 
-        System.out.println("testGetLowCaloricDishesName:" + names);
+    //函数的复合
+    @Test
+    public void testFunctionAndThenCompose() {
+        Function<Integer, Integer> f = (x) -> x + 1; //f(x) = x+1
+        Function<Integer, Integer> g = (x) -> 2 * x; //g(x) = 2* x
+        Function<Integer, Integer> h = f.andThen(g); //h(x) = g(f(x))
+        Function<Integer, Integer> i = f.compose(g); //i(x) = f(g(x))
+
+        Assert.assertTrue(h.apply(2) == 6);
+        Assert.assertTrue(i.apply(2) == 5);
+    }
+
+    // 比较器链：先按热量倒序排列，再按名字正序排列
+    @Test
+    public void testComparatorChain() {
+        menu.sort(Comparator.comparing(Dish::getCalories)
+                .reversed()
+                .thenComparing(Dish::getName));
+        System.out.println(menu);
+    }
+
+    // 谓词（Predicate）复合
+    @Test
+    public void testPredicateChain() {
+        Predicate<Dish> fish = dish -> dish.getType().equals(Dish.Type.FISH);
+        Predicate<Dish> lowCalories = dish -> dish.getCalories() < 400;
+
+        //非鱼类且低能量：[rice, season fruit]
+        System.out.println(menu.stream().filter(fish.negate().and(lowCalories)).collect(Collectors.toList()));
+
+        //鱼类或低能量：[rice, season fruit, prawns, salmon]
+        System.out.println(menu.stream().filter(fish.or(lowCalories)).collect(Collectors.toList()));
     }
 
     @Test
-    public void testDistinct() {
-        List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
-        System.out.println("testDistinct:");
-        numbers.stream().distinct().forEach(System.out::println);
+    public void testOperation() {
+        //filter:谓词过滤 [french fries, rice, season fruit, pizza]
+        System.out.println(menu.stream().filter(Dish::isVegetarian).collect(Collectors.toList()));
+        //distinct：去重 [1, 2, 3, 4, 5]
+        System.out.println(Arrays.asList(1, 2, 2, 3, 4, 5).stream().distinct().collect(Collectors.toList()));
+        //limit：截断 [french fries, rice]
+        System.out.println(menu.stream().filter(Dish::isVegetarian).limit(2).collect(Collectors.toList()));
+        //skip：跳过 [rice, season fruit, pizza]
+        System.out.println(menu.stream().filter(Dish::isVegetarian).skip(1).collect(Collectors.toList()));
+
+        //map：映射为新的集合
+        //[pork:800, beef:700, chicken:400, french fries:530, rice:350, season fruit:120, pizza:550, prawns:300, salmon:450]
+        System.out.println(menu.stream().map(dish -> dish.getName() + ":" + dish.getCalories()).collect(Collectors.toList()));
+
+        //flatMap:用于输入本身就是一个流的情况 [h, e, l, o, w, r, d]
+        System.out.println(Arrays.asList("hello", "word").stream().map(s -> s.split("")).flatMap(Arrays::stream)
+                .distinct().collect(Collectors.toList()));
+
+        //allMatch：所有素食能量都小于300时为真 false
+        System.out.println(menu.stream().filter(Dish::isVegetarian).allMatch(dish -> dish.getCalories() < 300));
+
+        //anyMatch：存在能量小于300的素食时为真 true
+        System.out.println(menu.stream().filter(Dish::isVegetarian).anyMatch(dish -> dish.getCalories() < 300));
+
+        //noneMatch：所有素食能量都小于300时为真 false
+        System.out.println(menu.stream().filter(Dish::isVegetarian).noneMatch(dish -> dish.getCalories() < 300));
+
+        //findAny: 找到任意素食 french fries
+        menu.stream().filter(Dish::isVegetarian).findAny().ifPresent(System.out::println);
+
+        //findAny: 找到第一个素食 french fries
+        menu.stream().filter(Dish::isVegetarian).findFirst().ifPresent(System.out::println);
+
+        //forEach: 打印每个元素，无返回值
+        menu.stream().forEach(System.out::println);
+
+        //reduce: 传入初始值和操作---求所有食物的热量之和 4200
+        System.out.println(menu.stream().map(Dish::getCalories).reduce(0, (a, b) -> a + b));
+
+        //reduce: 不传入初值，返回Optional对象---求热量最大的元素 800
+        menu.stream().map(Dish::getCalories).reduce(Integer::max).ifPresent(System.out::println);
     }
 
     @Test
-    public void testLimit() {
-        new File("/").listFiles(File::isHidden);
-        System.out.println("testLimit:");
-        menu.stream().filter(dish -> dish.getCalories() > 300).limit(3).forEach(System.out::println);
-    }
-
-    @Test
-    public void testSkip() {
-        System.out.println("testSkip:");
-        menu.stream().filter(dish -> dish.getCalories() > 300).skip(2).forEach(System.out::println);
-    }
-
-    // output:[h, e, l, o, w, r, d]
-    @Test
-    public void testFlatMap() {
-        List list = Arrays.asList("hello", "world").stream().map(s -> s.split("")) // 得到Stream<String[]>
-                .flatMap(Arrays::stream) // Arrays.stream得到Stream<Stream<String>>,flatMap得到Stream<String>
-                .distinct().collect(Collectors.toList());
-        System.out.println(list);
-    }
-
-    // output:[1, 3][1, 4][2, 3][2, 4][3, 3][3, 4]
-    @Test
-    public void testFlatMap2() {
-        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
-        List<Integer> numbers2 = Arrays.asList(3, 4);
-
-        List<int[]> results = numbers1.stream().flatMap(i -> numbers2.stream().map(j -> new int[] { i, j }))
-                .collect(Collectors.toList());
-        results.stream().map(array -> Arrays.toString(array)).forEach(System.out::println);
-    }
-
-    // output: true
-    @Test
-    public void testAnyMatch() {
-        System.out.println(menu.stream().anyMatch(Dish::isVegetarian));
-    }
-
-    // output:false
-    @Test
-    public void testAllMatch() {
-        System.out.println(menu.stream().allMatch(Dish::isVegetarian));
-    }
-
-    // output:false
-    @Test
-    public void testNoneMatch() {
-        System.out.println(menu.stream().noneMatch(Dish::isVegetarian));
-    }
-
-    @Test
-    public void testFind() {
-        // findAny output:french fries 并行效率更高
-        System.out.println(menu.stream().filter(Dish::isVegetarian).findAny().get());
-
-        // findFirst output:french fries
-        System.out.println(menu.stream().filter(Dish::isVegetarian).findFirst().get());
-    }
-
-    @Test
-    public void testReduce() {
-        List<Integer> numbers = Arrays.asList(4, 5, 3, 9);
-        System.out.println(numbers.stream().reduce(1, (a, b) -> a * b));
-        System.out.println(numbers.stream().reduce(0, (a, b) -> a + b));
-    }
-
-    @Test
-    public void testMaxMin() {
-        List<Integer> numbers = Arrays.asList(4, 5, 3, 9);
-        System.out.println(numbers.stream().reduce(Integer::max).get());
-        System.out.println(numbers.stream().reduce(Integer::min).get());
-    }
-
-    @Test
-    public void testJoinUrl() {
-        TreeMap<String, String> map = new TreeMap<>();
-        map.put("c", "1");
-        map.put("b", "2");
-        map.put("a", "3");
-        String ss = map.entrySet().stream().map(s -> s.getKey() + "=" + s.getValue()).collect(Collectors.joining("&"));
-        System.out.println(ss);
+    public void testIntStream() {
+        System.out.println(IntStream.rangeClosed(1, 100).filter(value -> value % 2 == 0).count());
+        IntStream.rangeClosed(1, 100).boxed()
+                .flatMap(a -> IntStream.rangeClosed(a, 100)
+                        .mapToObj(b -> Arrays.asList(a, b, Math.sqrt(a * a + b * b)))
+                        .filter(t -> t.get(2).doubleValue() % 1 == 0))
+                .forEach(System.out::println);
     }
 
 }
