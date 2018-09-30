514f6b9285ca275ba92c234b716454609745ef63
zhengxiaoyue <zhengxiaoyue@dpandora.cn>
2018-09-30 17:36:08 +0800

    regex
    Signed-off-by: zhengxiaoyue <zhengxiaoyue@dpandora.cn>

---
+++ src/main/java/com/cikezxy/sandbox/java8/forkjoin/CountTask.java
@@ -0,0 +1,15 @@
+package com.cikezxy.sandbox.java8.forkjoin;
+
+import java.util.concurrent.RecursiveTask;
+
+public class CountTask extends RecursiveTask<Integer> {
+
+    private static final int THRESHOLD = 10_000_000;
+    private int start;
+    private int end;
+
+    @Override
+    protected Integer compute() {
+        return null;
+    }
+}
